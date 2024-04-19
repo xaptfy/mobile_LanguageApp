29a63382db6f816ee18b184d00c6f32123a424f3
Arina <arina.bik30@gmail.com>
2024-04-19 14:30:35 +0300

    finish

---
+++ LanguageApp/app/src/main/java/ru/languageapp/LauncherActivity.kt
@@ -0,0 +1,23 @@
+package ru.languageapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import ru.languageapp.databinding.ActivityLauncherBinding
+class LauncherActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLauncherBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLauncherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+}
